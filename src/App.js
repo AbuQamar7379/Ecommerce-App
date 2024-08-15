e8df81aec270a5054eb76123a9e2b6c330c5f702
Rutik <rutikkulkarni2001@gmail.com>
2024-08-15 16:11:27 +0530

    Created files of the Components, Pages And heplers

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from "react";
 
 function App() {
   return (
     <div className="text-center">
       <header className="bg-gray-800 text-white flex flex-col items-center justify-center min-h-screen text-3xl">
-        <img src={logo} className="pointer-events-none h-40 animate-spin" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
